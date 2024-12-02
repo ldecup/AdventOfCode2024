acb31538666d0504820820d31854b0ffa07c2c25
ldecup <>
2024-12-02 10:25:49 +0100

    Initial commit

---
+++ DayXX/main.py
@@ -0,0 +1,16 @@
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
+logger = logging.getLogger(__name__)
+
+inPath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                      'testinput.txt')
+inFile = open(inPath, 'r')
+inData = inFile.readlines()
+inFile.close()
+
+data = []
+for line in inData:
+    data.append(line.rstrip('\n'))
+logger.info("Successfully imported {len(data)} lines of data")
