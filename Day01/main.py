b03bc899efb8323759800b7685f7dcabc878cad5
ldecup <>
2024-12-02 15:13:20 +0100

    Day 1 with a hashmap

--- Day01/main.py
+++ Day01/main.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from collections import Counter
 
 logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
 logger = logging.getLogger(__name__)
@@ -12,9 +13,11 @@ inFile.close()
 
 leftList = []
 rightList = []
+rightCounter = Counter()
 for line in inData:
     leftList.append(int(line.split()[0]))
     rightList.append(int(line.split()[1]))
+    rightCounter[int(line.split()[1])] += 1
 logger.info(f"Successfully imported {len(leftList)} lines of data")
 
 leftList.sort()
@@ -24,7 +27,8 @@ totalDistance = 0
 similarityScore = 0
 for leftNum, rightNum in zip(leftList, rightList):
     totalDistance += abs(rightNum - leftNum)
-    similarityScore += leftNum * rightList.count(leftNum)
+    similarityScore += leftNum * rightCounter[leftNum]   
+
 
 logger.info(f"Final distance for part 1: {totalDistance}")
 logger.info(f"Similarity score for part 2: {similarityScore}")
